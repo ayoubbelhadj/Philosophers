ea7ad79888ff6ba996f89323f4c8e8c2f1b8317e
abelhadj <abelhadj@student.1337.ma>
2023-04-03 21:39:15 +0000

    PHILO 5.0

--- philo/diner/ft_diner_end.c
+++ philo/diner/ft_diner_end.c
@@ -6,38 +6,25 @@
 /*   By: abelhadj <abelhadj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/29 03:09:03 by abelhadj          #+#    #+#             */
-/*   Updated: 2023/04/03 02:00:41 by abelhadj         ###   ########.fr       */
+/*   Updated: 2023/04/03 21:38:26 by abelhadj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
-
-int	ft_eat_check(t_philo *philo)
-{
-	if (philo->data->nbr_must_eat > 0)
-	{
-		if (!philo->finish && philo->eat >= philo->data->nbr_must_eat)
-		{
-			philo->data->finish++;
-		}
-		if (philo->data->finish >= philo->data->nbr_philo)
-		{
-			philo->data->end = 1;
-			printf("\033[0;37m	THE %d PHILOSOPHERS HAVE EAT %d TIME.\n",
-				philo->data->nbr_philo, philo->data->nbr_must_eat);
-			return (1);
-		}
-	}
-	return (0);
-}
 
 void	ft_diner_end(t_philo *philo)
 {
 	while (1)
 	{
 		pthread_mutex_lock(&philo->data->eat);
-		if (ft_eat_check(philo))
+		if (philo->data->nbr_must_eat > 0
+			&& philo->data->finish >= philo->data->nbr_philo)
+		{
+			philo->data->end = 1;
+			printf("\033[0;37m	THE %d PHILOSOPHERS HAVE EAT %d TIME.\n",
+				philo->data->nbr_philo, philo->data->nbr_must_eat);
 			return ;
+		}
 		pthread_mutex_unlock(&philo->data->eat);
 		pthread_mutex_lock(&philo->data->die);
 		if (philo->data->time_to_die <= (ft_get_time() - philo->last_eat)
--- philo/diner/ft_eating.c
+++ philo/diner/ft_eating.c
@@ -6,7 +6,7 @@
 /*   By: abelhadj <abelhadj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/24 01:19:50 by abelhadj          #+#    #+#             */
-/*   Updated: 2023/04/03 00:18:50 by abelhadj         ###   ########.fr       */
+/*   Updated: 2023/04/03 03:13:35 by abelhadj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,8 @@ void	ft_eating(t_philo *philo)
 	pthread_mutex_unlock(&philo->data->die);
 	pthread_mutex_lock(&philo->data->eat);
 	philo->eat++;
+	if (philo->eat == philo->data->nbr_must_eat)
+		philo->data->finish++;
 	pthread_mutex_unlock(&philo->data->eat);
 	ft_usleep(philo->data->time_to_eat);
 }
